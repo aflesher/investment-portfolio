f745da748dd3225cfe6019fe298118eadb06092d
Adam Flesher <adam.flesher@gmail.com>
2020-05-17 19:00:41 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: `@typescript-eslint/parser`,
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:react/recommended"
+  ],
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    quotes: ["error", "single"],
+    indent: ["error", "tab"],
+    semi: ["error", "always"],
+    "max-len": [1, { code: 120 }],
+    "ban-ts-ignore": 0,
+    "@typescript-eslint/ban-ts-ignore": 0,
+    "@typescript-eslint/interface-name-prefix": 0,
+    "react/prop-types": [0]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+}
