6d07e578514a81b12b7cd220142c7aab39764a2f
Adam Flesher <adam@getepic.com>
2021-06-06 11:07:43 -0700

    Remove debugging

--- src/pages/admin/crypto-trades.tsx
+++ src/pages/admin/crypto-trades.tsx
@@ -90,7 +90,6 @@ const CryptoTrades: React.FC<ICryptoTradesQuery & ICryptoTradeStateProps> = ({ f
 	};
 
     React.useEffect(() => {
-        console.log(firebase);
 		if (!firestore && firebase && firebase.firestore && user) {
 			const db = firebase.firestore();
 			setFirestore(db);
